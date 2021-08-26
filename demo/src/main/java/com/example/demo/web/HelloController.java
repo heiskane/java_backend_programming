3080b5c87fff0ab1604dd44dedca49f274962c84
heiskane <henipe@gmail.com>
2021-08-26 14:07:28 +0300

    added intro homework

---
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "This is the main page";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "This is the contact page";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(
+            @RequestParam String name,
+            @RequestParam(required = false, defaultValue = "World") String location
+    ) {
+        return "Welcome to the " + location + " " + name;
+    }
+}
