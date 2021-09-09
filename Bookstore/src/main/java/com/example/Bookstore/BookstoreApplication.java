66f94c65a96b8e5339fe7f7d3fae3fbacb2164dc
heiskane <henipe@gmail.com>
2021-09-09 13:58:43 +0300

    week3 exercise 1 done

--- Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,35 @@
 package com.example.Bookstore;
 
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			log.info("Commanline runner test");
+			repository.save(new Book("test book", "test author", 1996, "isbn123", 10F));
+			repository.save(new Book("test book2", "test author2", 1999, "isbn123", 20F));
+
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
 	}
 
 }
--- Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,16 +1,55 @@
 package com.example.Bookstore.web;
 
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class BookController {
 
-    @GetMapping("/index")
-    public String index() {
+    @Autowired
+    private BookRepository repository;
 
-        return "Hello";
+    @RequestMapping(value = {"/", "/booklist"})
+    public String books(Model model) {
 
+        model.addAttribute("books", repository.findAll());
+
+        return "booklist";
+    }
+
+    @RequestMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public String updateBook(@PathVariable("id") Long bookId, Model model) {
+
+        Optional<Book> book = repository.findById(bookId);
+
+        model.addAttribute("book", book);
+        return "addbook";
+    }
+
+    @RequestMapping("/save")
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:/booklist";
     }
 
 }
